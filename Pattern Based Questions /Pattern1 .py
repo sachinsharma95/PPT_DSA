6ef58dd7229265819b1952137377cc6267a2d321
SACHIN SHARMA <85471127+sachinsharma95@users.noreply.github.com>
2024-04-18 01:08:28 +0530

    Update Pattern1 .py

--- Pattern Based Questions /Pattern1 .py
+++ Pattern Based Questions /Pattern1 .py
@@ -43,4 +43,19 @@ def pattern3(n):
         for j in range(1,i+1):
             print(j,end="")
         print()
+
+Q4
+1
+22
+333
+4444
+55555
+
+Sol4
+
+def pattern4(n):
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print(i,end="")
+        print()
         
