412a0a704527126cc7e57d48300ee1a5d7147cfd
SACHIN SHARMA <85471127+sachinsharma95@users.noreply.github.com>
2024-04-19 12:16:34 +0530

    Update Pattern1 .py

--- Pattern Based Questions /Pattern1 .py
+++ Pattern Based Questions /Pattern1 .py
@@ -87,4 +87,30 @@ def pattern6(n):
         for j in range(1, n-i+1):
             print(j,end="")
         print()
+
+
+Q7  
+    
+      *    
+     ***   
+    *****  
+   ******* 
+  *********
+
+      # sol7
+
+      def pattern7(n):
+    for i in range(0,n):
+        for j in range(0,n-i+1):
+            print(" ",end="")
+            
+        for k in range(0,2*i+1):
+            print("*",end="")
+        for l in range(0,n-i-1):
+            print(" ", end="")
+        print()
         
+
+pattern7(5)
+            
+
